6b3af41852fc4a36c5b35d3f84fca33cb50e1a8a
qingyuyijiu <2814348231@qq.com>
2024-10-16 14:19:09 +0800

    删除了示例程序文件 1。

--- code/1.c
+++ code/1.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int function(char *ptr)
+{
+  char *p = ptr;
+  int ans = 0;
+  printf("%s\n", p);
+  if (*p != ' ') // 第一个不是空格则标记为一个单词
+  {
+    ans++;
+    p++;
+  }
+  while (*p != '\0' && p)
+  {
+    if (*p == ' ') // 遍历到空格
+    {
+      while (*p == ' ')
+      {
+        p++;
+      }
+      if (p && *p) // 没有空格且未到字符串结尾就标记为一个单词
+        ans++;
+    }
+    else
+      p++;
+  }
+  return ans;
+}
 
 int main()
 {
-  printf("Hello World!\n");
+  char str[1024] = {0};
+  gets(str);
+
+  printf("%d\n", function(str));
   return 0;
 }
