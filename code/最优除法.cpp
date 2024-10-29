d1226fab6a294689fe00c9288ddc80d306d641c1
qingyuyijiu <2814348231@qq.com>
2024-10-29 20:05:33 +0800

    ok

---
+++ code/最优除法.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+/*
+553. 最优除法
+中等
+相关标签
+相关企业
+给定一正整数数组 nums，nums 中的相邻整数将进行浮点除法。
+
+例如，nums = [2,3,4]，我们将求表达式的值 "2/3/4"。
+但是，你可以在任意位置添加任意数目的括号，来改变算数的优先级。你需要找出怎么添加括号，以便计算后的表达式的值为最大值。
+
+以字符串格式返回具有最大值的对应表达式。
+
+注意：你的表达式不应该包含多余的括号。
+*/
+
+class Solution
+{
+public:
+  string optimalDivision(vector<int> &nums)
+  {
+  }
+};
+
+int main()
+{
+
+  return 0;
+}
